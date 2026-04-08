e79a85a4c90a2a5a34414afb78d3a3067df4a395
AwonGirum <agir0006@student.monash.edu>
2026-04-08 11:11:24 +1000

    Update code with new pcb pins and led

--- nodes/node2-brakes/Brakes/Brakes.ino
+++ nodes/node2-brakes/Brakes/Brakes.ino
@@ -1,15 +1,21 @@
-#include "CAN_MREx.h"
+#include <CAN_MREx.h>
+#include <Adafruit_NeoPixel.h>
 
 uint8_t nodeID = 2; // brakes node id is 2
 
-#define TX_GPIO_NUM GPIO_NUM_18
-#define RX_GPIO_NUM GPIO_NUM_19
-#define SERVICE_BRAKE_PIN GPIO_NUM_22  // HIGH energises relay -> 24V to coil -> Release brakes
-#define  LED_LIGHT GPIO_NUM_33
+#define TX_GPIO_NUM GPIO_NUM_8 //18
+#define RX_GPIO_NUM GPIO_NUM_7 //19
+#define SERVICE_BRAKE_PIN GPIO_NUM_9  // 22 HIGH energises relay -> 24V to coil -> Release brakes
+//LED pins
+
+#define  LED_LIGHT GPIO_NUM_38
+#define LED_COUNT 1
+Adafruit_NeoPixel pixel(LED_COUNT, LED_LIGHT, NEO_GRB + NEO_KHZ800);
+
 uint8_t lastOut = 255 ;// remembers what the brake output was last time
 // manually set it high at the start because when the code runs it will se a change and print a message
 
-uint8_t mcServiceBrakeRequest = 1; // 1 brakes on 0 brakes off
+// testing
 
 
 void setup() {
@@ -18,49 +24,70 @@ void setup() {
   Serial.println("Brakes node started (Node 2)");
 
   initCANMREX(TX_GPIO_NUM, RX_GPIO_NUM, nodeID);
-  xTaskCreatePinnedToCore(
-    CAN_Task,
-    "CAN Task",
-    4096,
-    &nodeID,   // <--- passed into pvParameters
-    3,
-    NULL,
-    0
-  );
 
   pinMode(SERVICE_BRAKE_PIN, OUTPUT);
-  pinMode(LED_LIGHT, OUTPUT);
+  // pinMode(LED_LIGHT, OUTPUT);
+  //onboard LED setup
+  pixel.begin();
+  pixel.clear();
+  pixel.show();
+
 
   // Fail-safe startup: APPLY brakes in the beginning (no power to coil)
   digitalWrite(SERVICE_BRAKE_PIN, LOW);
 
-  registerODEntry(0x3012, 0x01, 2, sizeof(mcServiceBrakeRequest), &mcServiceBrakeRequest);
-
   // IMPORTANT: Do NOT force operational mode here. ONLY for testing
   // nodeOperatingMode should be controlled by NMT messages on the CAN network.
   // nodeOperatingMode = 0x01; 
+
+  xTaskCreatePinnedToCore(
+      CAN_Task,
+      "CAN Task",
+      6144,
+      &nodeID,
+      3,
+      NULL,
+      0
+  );
+
+
+ 
+ 
+
+
 }
+
+
 
 void loop() {
   // Always process CAN traffic (including NMT state changes)
+  // handleCAN(nodeID);
 
   uint8_t out; // variable for brake pin output
 
   // STOPPED (0x02) or PRE-OP (0x80) => APPLY friction brakes => relay OFF
   if (nodeOperatingMode == 0x02 || nodeOperatingMode == 0x80) {
     out = LOW;   // relay de-energised -> no 24V -> brake APPLIED
-    digitalWrite(LED_LIGHT,LOW);
+    // digitalWrite(LED_LIGHT,LOW);
+    pixel.setPixelColor(0, pixel.Color(50, 0, 0)); // red = applied
+    pixel.show();
 
   }
   // OPERATIONAL (0x01) => RELEASE friction brakes => relay ON
-  else if (nodeOperatingMode == 0x01 && mcServiceBrakeRequest == 0) {
+  else if (nodeOperatingMode == 0x01) {
     out = HIGH;  // relay energised -> 24V -> brake RELEASED
-    digitalWrite(LED_LIGHT,HIGH);
+    // digitalWrite(LED_LIGHT,HIGH);
+    pixel.setPixelColor(0, pixel.Color(0, 50, 0)); // green = released
+    pixel.show();
+
   }
   // Unknown state => fail-safe APPLY
   else {
     out = LOW;
-    digitalWrite(LED_LIGHT,HIGH);
+    // digitalWrite(LED_LIGHT,HIGH);
+    pixel.setPixelColor(0, pixel.Color(50, 0, 0)); // red = applied
+    pixel.show();
+
   }
 
   // Only update + print when it changes, to make seeing changes more clear
