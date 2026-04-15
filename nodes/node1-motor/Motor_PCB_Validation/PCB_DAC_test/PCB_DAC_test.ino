74e94085472b7351a56bd23cec7203f64fbc356d
Chiara <chiaragillam@gmail.com>
2026-04-15 15:11:55 +1000

    Update PCB_DAC_test.ino

--- nodes/node1-motor/Motor_PCB_Validation/PCB_DAC_test/PCB_DAC_test.ino
+++ nodes/node1-motor/Motor_PCB_Validation/PCB_DAC_test/PCB_DAC_test.ino
@@ -1,16 +1,17 @@
 /**
- * PCB DAC test  file 
+ * PCB DAC test file
  *
  * File:            PCB_DAC_test.ino
  * Organisation:    MREX
  * Author:          Chiara Gillam
  * Date Created:    13/03/2026
  * Last Modified:   13/03/2026
- * Version:         1.0.0
+ * Version:         1.0.1
  *
+ * Description:
+ *   Tests the DAC outputs by generating independent ramp signals
+ *   on VOUT0 and VOUT1.
  */
-//Tests the dac output 
-
 
 #include <Arduino.h>
 #include <Wire.h>
@@ -19,27 +20,36 @@
 #define SDA_PIN  GPIO_NUM_2
 #define SCL_PIN  GPIO_NUM_1
 
-const int maxDAC = 1023;   // 10-bit
+const int maxDAC = 1023;   // 10‑bit DAC
 const int stepSize = 10;
 const int stepDelay = 20;
 
-int dacValue0 = 1022;
-int dacValue1 = 0;
-bool rampUp = true;
+int dacValue0 = 0;
+int dacValue1 = 1023;
 
+bool rampUp0 = true;
+bool rampUp1 = false;
+
+// ------------------------------------------------------------
+// Configure DAC: enable internal reference, enable VOUT0/1
+// ------------------------------------------------------------
 void commonConfig() {
   Wire.beginTransmission(DAC_ADDR);
   Wire.write(0x1F);   // COMMON-CONFIG register
-  Wire.write(0x02);   // EN-INT-REF=0, VOUT0 ON, IOUT0 OFF
+  Wire.write(0x02);   // VOUT0 ON, IOUT0 OFF
   Wire.write(0x01);   // VOUT1 ON, IOUT1 OFF
   Wire.endTransmission();
 }
 
+// ------------------------------------------------------------
+// Write 10‑bit DAC value to channel 0 or 1
+// ------------------------------------------------------------
 void writeDAC(uint8_t channel, uint16_t value) {
   if (value > 1023) value = 1023;
-  uint16_t regVal = value << 6;                 // left-align 10-bit
 
-  uint8_t reg = (channel == 0) ? 0x1C : 0x19;   // DAC-0-DATA / DAC-1-DATA
+  uint16_t regVal = value << 6;  // left‑align 10‑bit into 16‑bit
+
+  uint8_t reg = (channel == 0) ? 0x1C : 0x19; // DAC‑0‑DATA / DAC‑1‑DATA
 
   Wire.beginTransmission(DAC_ADDR);
   Wire.write(reg);
@@ -48,6 +58,28 @@ void writeDAC(uint8_t channel, uint16_t value) {
   Wire.endTransmission();
 }
 
+// ------------------------------------------------------------
+// Update ramp value for a channel
+// ------------------------------------------------------------
+void updateRamp(int &value, bool &direction) {
+  if (direction) {
+    value += stepSize;
+    if (value >= maxDAC) {
+      value = maxDAC;
+      direction = false;
+    }
+  } else {
+    value -= stepSize;
+    if (value <= 0) {
+      value = 0;
+      direction = true;
+    }
+  }
+}
+
+// ------------------------------------------------------------
+// Setup
+// ------------------------------------------------------------
 void setup() {
   Serial.begin(115200);
   delay(1000);
@@ -55,47 +87,20 @@ void setup() {
 
   Wire.begin(SDA_PIN, SCL_PIN);
 
-  commonConfig();   // power up VOUT0/1, enable internal ref
+  commonConfig();
 }
 
+// ------------------------------------------------------------
+// Main loop
+// ------------------------------------------------------------
 void loop() {
   writeDAC(0, dacValue0);
   writeDAC(1, dacValue1);
 
-  Serial.print("DAC Value0: ");
-  Serial.println(dacValue0);
-  Serial.print("DAC Value1: ");
-  Serial.println(dacValue1);
+  Serial.printf("DAC0: %d   DAC1: %d\n", dacValue0, dacValue1);
 
-
-  
-  // if (rampUp) {
-  //   dacValue0 += stepSize;
-  //   if (dacValue0 >= maxDAC) {
-  //     dacValue0 = maxDAC;
-  //     rampUp = false;
-  //   }
-  // } else {
-  //   dacValue0 -= stepSize;
-  //   if (dacValue0 <= 0) {
-  //     dacValue0 = 0;
-  //     rampUp = true;
-  //   }
-  // }
-
-  // if (rampUp) {
-  //   dacValue1 += stepSize;
-  //   if (dacValue1 >= maxDAC) {
-  //     dacValue1 = maxDAC;
-  //     rampUp = false;
-  //   }
-  // } else {
-  //   dacValue1 -= stepSize;
-  //   if (dacValue1 <= 0) {
-  //     dacValue1 = 0;
-  //     rampUp = true;
-  //   }
-  // }
+  updateRamp(dacValue0, rampUp0);
+  updateRamp(dacValue1, rampUp1);
 
   delay(stepDelay);
 }
